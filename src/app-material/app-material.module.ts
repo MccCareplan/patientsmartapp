f97d0b7a0968bd01cc4f4135ecf9be71a7e63703
Stephen Maier <smaier@cognitivemedicine.com>
2020-11-04 16:12:55 -0800

    Added PWA

--- src/app-material/app-material.module.ts
+++ src/app-material/app-material.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule} from '@angular/material/button';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatInputModule,
-    MatButtonModule
-  ],
-  exports: [
-    MatInputModule,
-    MatButtonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatToolbarModule
+    ],
+    exports: [
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatToolbarModule
+    ]
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppMaterialModule} from '../app-material/app-material.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -12,7 +14,8 @@ import {AppMaterialModule} from '../app-material/app-material.module';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        AppMaterialModule
+        AppMaterialModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [],
     bootstrap: [AppComponent]
