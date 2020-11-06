10258d8d00218b31f3da332b21c95be3de8fb788
Stephen Maier <smaier@cognitivemedicine.com>
2020-11-06 14:53:59 -0800

    added header and footer components/icons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,32 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './app-material/app-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { MainComponent } from './main/main.component';
 import { LaunchComponent } from './launch/launch.component';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
+import { HeaderMobileComponent } from './navigation/header-mobile/header-mobile.component';
+import { FooterMobileComponent } from './navigation/footer-mobile/footer-mobile.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         MainComponent,
-        LaunchComponent
+        LaunchComponent,
+        HeaderMobileComponent,
+        FooterMobileComponent,
+        SidenavComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         AppMaterialModule,
+        FlexLayoutModule,
         ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
         AppRoutingModule
     ],
