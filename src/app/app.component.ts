70d1eab2609046cc5ab02e37d9f489eeccfda2a8
Stephen Maier <stevemaier@rocketmail.com>
2020-11-06 15:30:45 -0800

    Develop (#26)
    * Feature 3 pwa support (#22)
    
    * added .gitignore
    
    * Updated project to angular 10.2
    
    * Added Angular Material
    
    * Added Angular Material
    
    * Added PWA
    
    * Added PWA support
    
    * Changed application name to patientsmartapp, configured start:pwa in package.json
    
    * Added main and launch components, routes (#23)
    
    * added header and footer components/icons. (#24)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'patientsmartapp1';
+  title = 'Patient Smart App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './app-material/app-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { MainComponent } from './main/main.component';
+import { LaunchComponent } from './launch/launch.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import { HeaderMobileComponent } from './navigation/header-mobile/header-mobile.component';
+import { FooterMobileComponent } from './navigation/footer-mobile/footer-mobile.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent,
+        LaunchComponent,
+        HeaderMobileComponent,
+        FooterMobileComponent,
+        SidenavComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppMaterialModule,
+        FlexLayoutModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+        AppRoutingModule
+    ],
+    providers: [],
+    exports: [RouterModule],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
