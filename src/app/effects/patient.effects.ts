8d4d9a4f6d6b73611f3db34f93fcf6b0de088341
Stephen Maier <smaier@cognitivemedicine.com>
2020-11-19 10:41:20 -0800

    working on patient.effects.ts

--- src/app/effects/patient.effects.ts
+++ src/app/effects/patient.effects.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType, Effect} from '@ngrx/effects';
-import { EMPTY } from 'rxjs';
+import {EMPTY, Observable, of} from 'rxjs';
 
 import {DataService} from '../services/data.service';
 import * as patient from '../actions/patient.actions';
-import { map, mergeMap, catchError } from 'rxjs/operators';
-
+import {map, mergeMap, catchError, startWith, concatMap, switchMap} from 'rxjs/operators';
+import {Action} from '@ngrx/store';
 
 
 @Injectable()
@@ -17,8 +17,8 @@ export class PatientEffects {
         switchMap(() => {
             return this.service.getAll()
                 .pipe(
-                map(patients => patient.LoadAllSuccessAction({data: patients}),
-                catchError(error => observableOf(patient.LoadAllFailAction(error)))));
+                    map(patients => patient.LoadAllSuccessAction({data: patients}),
+                        catchError(error => of(patient.LoadAllFailAction(error)))));
         }));
 
     loadAllEffect$: any = createEffect((): any => this.actions$.pipe(
@@ -29,14 +29,41 @@ export class PatientEffects {
                 .pipe(
                     map(patients => patient.LoadAllSuccessAction({data: patients})),
                     catchError(error =>
-                        observableOf(patient.LoadAllFailAction({ error }))
+                        of(patient.LoadAllFailAction({error}))
                     )
                 )
         )
         )
     );
 
+    @Effect()
+    load$: Observable<Action> = this.actions$.pipe(
+        ofType(patient.SELECT),
+        startWith(patient.selectAction({data: 'Patricia'}))
+        , map(toPayload)
+        , switchMap(payload => {
+            return this.service.getPatientProfile(payload).pipe(
+                switchMap(profile => [
+                    patient.LoadPatientSuccessAction({data: profile}),
+                    // new group.LoadAction(profile.userId)
+                ]),
+                    catchError((error) => of(new user.LoadProfileFailAction(error))));
+        }));
 
+    loadEffect$: any = createEffect((): any => this.actions$.pipe(
+        ofType(patient.SELECT),
+        startWith(patient.selectAction({data: 'Patricia'}))
+        , map(toPayload)
+        , switchMap(payload => {
+            return this.service.getPatientProfile(payload).pipe(
+                switchMap(profile => [
+                    new user.LoadProfileSuccessAction(profile),
+                    // new group.LoadAction(profile.userId)
+                ]),
+                    catchError((error) => of(new user.LoadProfileFailAction(error))));
+
+
+        })));
 
 
     constructor(
