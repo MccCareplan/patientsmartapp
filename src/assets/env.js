ee31770d1d718b71a41e047123b16bd86b38c533
aschutzman-cognitive <75691772+aschutzman-cognitive@users.noreply.github.com>
2021-01-12 12:49:27 -0500

    in progress

--- src/assets/env.js
+++ src/assets/env.js
@@ -5,7 +5,7 @@
   // clientId: "0950cf46-841c-466f-8bfb-19aa7d351b77",  //Public Server
   // clientId: "1491aa24-3b5b-42e8-b532-63707c359493", //Local Host Test
   //Maybe be replaced by the copy in src/deployment/production
-  window["env"]["clientId"] = "1491aa24-3b5b-42e8-b532-63707c359493";
+  window["env"]["clientId"] = "87c93d40-6d12-41fa-9a6f-87c951ff17e6";
   window["env"]["apiServer"] = "";
   window['env']['standaloneServer'] = "https://api.logicahealth.org/MCCeCarePlanTest/data";
 })(this);
