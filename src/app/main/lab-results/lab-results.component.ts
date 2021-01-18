3ffc139a36ab5c4d8e65f79850cf6e4a1fb13c1d
aschutzman-cognitive <75691772+aschutzman-cognitive@users.noreply.github.com>
2021-01-18 12:59:13 -0500

    Removed hard-coded test variables form lab results

--- src/app/main/lab-results/lab-results.component.ts
+++ src/app/main/lab-results/lab-results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { filter } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../ngrx/reducers';
+import { Constants } from 'src/app/common/constants';
 import { MccObservation } from 'src/app/generated-data-api';
 import { ObservationsService } from 'src/app/services/observations.service.new';
 
@@ -16,18 +18,11 @@ interface PatientLabResultsMap {
 })
 export class LabResultsComponent implements OnInit {
   results: MccObservation[] = [];
-  keyToObservations: Map<string, PatientLabResultsMap[]> = new Map<string, PatientLabResultsMap[]>();
-  // set key off of patientid and long term care condition
-  // use long term care condition as key to retrieve required observations
-  // split required observations into 3 groups: panel, valueset and value
-  // call 3 groups individually for custom success handlers
-  // in each handler, format if necessary
-  // required output is: "name", "value", "date"
-
-  patientId: string = "cc-pat-pnoelle";
-  longTermCondition: string = "ckd";
+  patientId: string;
+  longTermCondition: string;
 
   constructor(
+    private store: Store<fromRoot.State>,
     private obsService: ObservationsService
   ) {
 
@@ -38,9 +33,23 @@ export class LabResultsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.testData();
+    this.store.select(fromRoot.getCarePlansSummary).subscribe(c => {
+      if (c && c.length > 0) {
+        this.longTermCondition = c[0].fhirid.split("-")[3];
+        if (this.patientId && this.longTermCondition) this.loadData();
+      }
+    });
+    this.store.select(fromRoot.getPatientProfile).subscribe(x => {
+      if (x && x.fhirid) {
+        this.patientId = x.fhirid;
+        if (this.patientId && this.longTermCondition) this.loadData();
+      }
+    });
+  }
+
+  loadData = (): void => {
     this.results = [];
-    let callsToMake: PatientLabResultsMap[] = this.keyToObservations.get(this.longTermCondition);
+    let callsToMake: PatientLabResultsMap[] = Constants.labResultsMap.get(this.longTermCondition);
     let promiseArray = [];
     callsToMake.forEach((v, i) => {
       switch (v.type) {
@@ -80,131 +89,5 @@ export class LabResultsComponent implements OnInit {
     if (res.length < 1)
       return false;
     return true;
-  }
-
-  testData() {
-    this.keyToObservations.set(this.longTermCondition, [
-      {
-        name: "Blood Pressure",
-        value: "85354-9",
-        type: "panel"
-      },
-      {
-        name: "Weight",
-        value: "29463-7",
-        type: "panel"
-      },
-      {
-        name: "eGFR",
-        value: "2.16.840.1.113762.1.4.1222.179",
-        type: "valueset"
-      },
-      {
-        name: "Serum Creatinine",
-        value: "2.16.840.1.113762.1.4.1222.111",
-        type: "valueset"
-      },
-      ,
-      {
-        name: "Hemoglobin",
-        value: "2.16.840.1.113762.1.4.1222.114",
-        type: "valueset"
-      },
-      {
-        name: "A1C",
-        value: "2.16.840.1.113762.1.4.1222.119",
-        type: "valueset"
-      },
-      {
-        name: "Serum Potassium",
-        value: "2.16.840.1.113762.1.4.1222.120",
-        type: "valueset"
-      },
-      {
-        name: "Serum phosphorus",
-        value: "2.16.840.1.113762.1.4.1222.123",
-        type: "valueset"
-      },
-      {
-        name: "Calcium",
-        value: "17861-6",
-        type: "code"
-      },
-      {
-        name: "Corrected Calcium",
-        value: "2.16.840.1.113762.1.4.1222.122",
-        type: "valueset"
-      },
-      {
-        name: "Serum Albumin",
-        value: "2.16.840.1.113762.1.4.1222.151",
-        type: "valueset"
-      },
-      {
-        name: "Transferrin Staturation",
-        value: "2.16.840.1.113762.1.4.1222.118",
-        type: "valueset"
-      },
-      {
-        name: "Ferritin",
-        value: "2.16.840.1.113762.1.4.1222.140",
-        type: "valueset"
-      },
-      {
-        name: "Blood Urea Nitrogen",
-        value: "2.16.840.1.113762.1.4.1222.113",
-        type: "valueset"
-      },
-      {
-        name: "LDL",
-        value: "13457-7",
-        type: "code"
-      },
-      {
-        name: "HDL",
-        value: "2085-9",
-        type: "code"
-      },
-      {
-        name: "Triglycerides",
-        value: "2.16.840.1.113762.1.4.1222.137",
-        type: "valueset"
-      },
-      {
-        name: "Total Cholesterol",
-        value: "2.16.840.1.113762.1.4.1222.139",
-        type: "valueset"
-      },
-      {
-        name: "Vitamin D",
-        value: "2.16.840.1.113762.1.4.1222.126",
-        type: "valueset"
-      },
-      {
-        name: "KT/V",
-        value: "2.16.840.1.113762.1.4.1222.128",
-        type: "valueset"
-      },
-      {
-        name: "Intact Parathyroid Hormone",
-        value: "2.16.840.1.113762.1.4.1222.129",
-        type: "valueset"
-      },
-      {
-        name: "Serum Biacarbonate",
-        value: "2.16.840.1.113762.1.4.1222.130",
-        type: "valueset"
-      },
-      {
-        name: "Sodium",
-        value: "2.16.840.1.113762.1.4.1222.131",
-        type: "valueset"
-      },
-      {
-        name: "Chloride",
-        value: "2.16.840.1.113762.1.4.1222.132",
-        type: "valueset"
-      },
-    ])
   }
 }
