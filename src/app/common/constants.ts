2c2d8615cf7524b84d43e9e2497aa870aabf95c9
aschutzman-cognitive <75691772+aschutzman-cognitive@users.noreply.github.com>
2021-05-04 11:41:29 -0400

    graph lifecycle workaround

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -3,4 +3,6 @@ export class Constants {
     public static featureToggling: any;
     public static labMappings: any;
     public static vitalMappings: any;
+    static EGFRisLoaded: string = "EGFR-is-Loaded-patient-app-1";
+    static UACRisLoaded: string = "UACR-is-Loaded-patient-app-1";
 }
